e918e323a7a55c8bcec001e2fb2d1130a0ce0654
arnoud thörig <arnoudt@MacBook-Pro-2.local>
2019-05-09 11:23:41 +0200

    tasks copy zip sass, joint to one task  made

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,39 @@
 /*global module:false*/
+const sass = require('node-sass');
+
 module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
-    zip: {'dist/bundle.zip' : ['index.html', 'source-files/script.js', 'source-files/style.scss']}
-
+    zip: {'dist/bundle.zip' : ['index.html', 'assets/script.js', 'assets/style.css']},
+    sass: {
+      options: {
+        implementation: sass,
+        sourceMap: false
+      },
+      dist: {
+        files: {
+          'assets/style.css': 'source-files/style.scss'
+        }
+      }
+    },
+    copy: {
+      main: {
+        expand: true,
+        cwd: 'source-files',
+        src: '*.js',
+        dest: 'assets/',
+      },
+    },
   });
 
   // Load tasks here.
   grunt.loadNpmTasks('grunt-zip');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   // Define aliases here.
-  grunt.registerTask('default', 'My default task description', function() {
-    grunt.log.writeln( 'This is the default grunt task, create a new task and configure.' );
-  });
+  grunt.registerTask('default', 'My default task description', ['sass', 'copy', 'zip']);
 
 };
