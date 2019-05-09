0dbedcf6d0338c38d9d6f5228fc1173338ac1fdc
arnoud thörig <arnoudt@MacBook-Pro-2.local>
2019-05-09 09:45:55 +0200

    zip builder added to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,10 +5,12 @@ module.exports = function(grunt) {
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
+    zip: {'dist/bundle.zip' : ['index.html', 'source-files/script.js', 'source-files/style.scss']}
+
   });
 
   // Load tasks here.
-
+  grunt.loadNpmTasks('grunt-zip');
   // Define aliases here.
   grunt.registerTask('default', 'My default task description', function() {
     grunt.log.writeln( 'This is the default grunt task, create a new task and configure.' );
