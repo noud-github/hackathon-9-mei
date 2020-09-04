e3b1de73f1c50fe7645ce98a4fe332767dfba914
arnoud thörig <arnoud@yoast.com>
2020-09-04 15:52:31 +0200

    add svn mockup

---
+++ config/tools/svn.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import sys
+f = open("/tmp/log.txt","a+")
+f.write("---------------------------\n")
+for arg in sys.argv:
+    f.write(arg)
+    f.write("\n")
+
+if len(sys.argv)> 1:
+    if sys.argv[1] == 'co':
+        for arg in sys.argv:
+            if arg =='--username=Yoast':
+                # tag check needs to fail
+                sys.exit(1)
+        
+        
+sys.exit(0)
+f.close()
+
