c5d010a39aef75ebc68260a27325293ea129628d
arnoud thörig <arnoud@yoast.com>
2020-09-07 10:35:58 +0200

    add real svn test

--- config/tools/svn.py
+++ config/tools/svn.py
@@ -3,16 +3,17 @@ import sys
 f = open("/tmp/log.txt","a+")
 f.write("---------------------------\n")
 for arg in sys.argv:
-    f.write(arg)
-    f.write("\n")
-
+	f.write(arg)
+	f.write("\n")
+	
 if len(sys.argv)> 1:
-    if sys.argv[1] == 'co':
-        for arg in sys.argv:
-            if arg =='--username=Yoast':
-                # tag check needs to fail
-                sys.exit(1)
-        
+	if sys.argv[1] == 'co':
+		for arg in sys.argv:
+			if arg =='--username=Yoast':
+				# tag check needs to fail
+				sys.exit(1)
+			if arg =='--version':
+				print('Mockup version 1.1')
         
 sys.exit(0)
 f.close()
